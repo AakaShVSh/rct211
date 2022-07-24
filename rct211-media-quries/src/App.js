e33de2c43e2420dc8596448bb83623dda5600059
Aakash Vishwakarma <97448935+AakaShVSh@users.noreply.github.com>
2022-07-24 23:17:42 +0530

    Add files via upload

---
+++ rct211-media-quries/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <h2>Layouts</h2>
+   <div className="container">
+    <div className="first"></div>
+    <div className="middle"></div>
+    <div className="last"></div>
+   </div>
+    </div>
+  );
+}
+
+export default App;
